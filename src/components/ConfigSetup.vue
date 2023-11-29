174b25f4edd66c2fdffeb051bf0a4f459c1489f5
mozzarelladoublestick <92583850+mozzarelladoublestick@users.noreply.github.com>
2023-11-29 16:21:09 +0100

    comment

--- src/components/ConfigSetup.vue
+++ src/components/ConfigSetup.vue
@@ -335,6 +335,7 @@ const raycastObjects = [];
 
 let INTERSECTED;
 function onClick() {
+  //only loaded objects are selectable
   for (const key in loadedObjects) {
   if (Object.hasOwnProperty.call(loadedObjects, key)) {
     const object = loadedObjects[key];
